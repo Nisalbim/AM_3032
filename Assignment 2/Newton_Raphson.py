52bd9c32bff531a4877686f305418b62e1476cde
Nisalbim <103802560+Nisalbim@users.noreply.github.com>
2022-07-25 07:31:25 +0530

    Second Assignment

---
+++ Assignment 2/Newton_Raphson.py
@@ -0,0 +1,38 @@
+import sympy as sym #import sympy lib to do differentiations
+
+x = sym.symbols('x') #variable define
+f = x**3 - x -1      #function f define
+derv = sym.diff(f,x) #differentiate the function
+
+def f(x):
+    return x**3 - x -1
+
+#defining the given conditions
+x_0 = 1.5
+ep = 10 ** -4
+N=50
+
+#initial step carried out
+x_n = x_0
+error = f(x_n)
+
+i = 1
+k = 0
+
+#newton raphson method steps
+while error >= ep:
+    val = derv.subs(x, x_n) #derivative calculate
+    x_i = x_n - f(x_n)/val  #x_n+1 calculate
+    error = abs(f(x_i))     #error value |g(x)|
+    print(i, '   ', x_i, '   ', error)
+    if i > N:               #if precision not satisfying end the loop finite steps
+        print('for 50 iteration no precision satisfying value')
+        k = 1
+        break
+    x_n = x_i               #x values updating
+    i = i+1
+
+if k != 1:                  #print the root
+    print("root of the equation",x_i)
+
+
